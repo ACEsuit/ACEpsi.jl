70d030cab777a4fdcfab3ee4e5451fdfc7c85734
DexuanZhou <hbnis@icloud.com>
2023-06-05 08:36:05 +0800

    minor fix

--- test/test_bflow_lux.jl
+++ test/test_bflow_lux.jl
@@ -2,8 +2,7 @@ using ACEpsi, Polynomials4ML, StaticArrays, Test
 using Polynomials4ML: natural_indices, degree, SparseProduct
 using ACEpsi.AtomicOrbitals: AtomicOrbitalsBasis, Nuc, make_nlms_spec, ProductBasis, evaluate
 using ACEpsi: BackflowPooling, BFwf_lux, setupBFState, Jastrow
-using ACEbase.Testing: print_tf, fd_test
-
+using ACEbase.Testing: print_tf, fdtest
 using LuxCore
 using Lux
 using Zygote
