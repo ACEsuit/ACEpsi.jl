bf23e3baa20401857ff6d55fdcbfdb3848db5231
cheukhinhojerry <cheukhinhojerry@gmail.com>
2023-08-06 02:06:18 -0700

    add embedding

--- src/vmc/vmc.jl
+++ src/vmc/vmc.jl
@@ -4,64 +4,82 @@ using LinearAlgebra
 using Optimisers
 
 mutable struct VMC
-    tol::Number
-    MaxIter::Int
-    lr::Number
-    lr_dc::Number
-    type::opt
+   tol::Number
+   MaxIter::Int
+   lr::Number
+   lr_dc::Number
+   type::opt
 end
 
 VMC(MaxIter::Int, lr::Number, type; tol = 1.0e-3, lr_dc = 50.0) = VMC(tol, MaxIter, lr, lr_dc, type);
         
 function gd_GradientByVMC(opt_vmc::VMC, sam::MHSampler, ham::SumH, 
-                wf, ps, st, 
-                ν = 1, verbose = true, accMCMC = [10, [0.45, 0.55]])
+               wf, ps, st, 
+               ν = 1, verbose = true, accMCMC = [10, [0.45, 0.55]])
 
-    res, λ₀, α = 1.0, 0., opt_vmc.lr
-    err_opt = zeros(opt_vmc.MaxIter)
+   res, λ₀, α = 1.0, 0., opt_vmc.lr
+   err_opt = zeros(opt_vmc.MaxIter)
 
-    x0, ~, acc = sampler_restart(sam, ps, st)
-    acc_step, acc_range = accMCMC
-    acc_opt = zeros(acc_step)
+   x0, ~, acc = sampler_restart(sam, ps, st)
+   acc_step, acc_range = accMCMC
+   acc_opt = zeros(acc_step)
 
-    verbose && @printf("Initialize MCMC: Δt = %.2f, accRate = %.4f \n", sam.Δt, acc)
-    verbose && @printf("   k |  𝔼[E_L]  |  V[E_L] |   res   |   LR    |accRate|   Δt    \n")
-    for k = 1 : opt_vmc.MaxIter
-        sam.x0 = x0
-        
-        # adjust Δt
-        acc_opt[mod(k,acc_step)+1] = acc
-        sam.Δt = acc_adjust(k, sam.Δt, acc_opt, acc_range, acc_step)
+   verbose && @printf("Initialize MCMC: Δt = %.2f, accRate = %.4f \n", sam.Δt, acc)
+   verbose && @printf("   k |  𝔼[E_L]  |  V[E_L] |   res   |   LR    |accRate|   Δt    \n")
+   for k = 1 : opt_vmc.MaxIter
+      sam.x0 = x0
+      
+      # adjust Δt
+      acc_opt[mod(k,acc_step)+1] = acc
+      sam.Δt = acc_adjust(k, sam.Δt, acc_opt, acc_range, acc_step)
 
-        # adjust learning rate
-        α, ν = InverseLR(ν, opt_vmc.lr, opt_vmc.lr_dc)
+      # adjust learning rate
+      α, ν = InverseLR(ν, opt_vmc.lr, opt_vmc.lr_dc)
 
-        # optimization
-        ps, acc, λ₀, res, σ = Optimization(opt_vmc.type, wf, ps, st, sam, ham, α)
+      # optimization
+      ps, acc, λ₀, res, σ = Optimization(opt_vmc.type, wf, ps, st, sam, ham, α)
 
-        # err
-        verbose && @printf(" %3.d | %.5f | %.5f | %.5f | %.5f | %.3f | %.3f \n", k, λ₀, σ, res, α, acc, sam.Δt)
-        err_opt[k] = λ₀
+      # err
+      verbose && @printf(" %3.d | %.5f | %.5f | %.5f | %.5f | %.3f | %.3f \n", k, λ₀, σ, res, α, acc, sam.Δt)
+      err_opt[k] = λ₀
 
-        if res < opt_vmc.tol
-            break;
-        end  
-    end
-    return wf, err_opt, ps
+      if res < opt_vmc.tol
+         break;
+      end  
+   end
+   return wf, err_opt, ps
 end
 
 
-# function Embedding(wf, ps, st, deg, sd_admissible = x -> true)
-#     Σ = st.bA.Σ
+function _invmap(a::AbstractVector)
+   inva = Dict{eltype(a), Int}()
+   for i = 1:length(a) 
+      inva[a[i]] = i 
+   end
+   return inva 
+end
 
-#     # init new wf
-#     wf2 = BFwf1dps_lux(Nel, wf.layers.Pn.layers.layer_1.basis, totdeg = deg, sd_admissible = sd_admissible)
-#     ps2, st2 = setupBFState(MersenneTwister(1234), wf2, Σ)
+# TODO: this should be implemented to recursively embed the wavefunction
+function EmbeddingW!(ps, ps2, spec, spec2, spec1p, spec1p2)
+   readable_spec = ACEpsi.displayspec(spec, spec1p)
+   readable_spec2 = ACEpsi.displayspec(spec2, spec1p2)
+   @assert size(ps.hidden1.W, 2) == size(ps2.hidden1.W, 2)
+   @assert size(ps.hidden1.W, 1) ≤ size(ps2.hidden1.W, 1)
+   @assert all(t in readable_spec2 for t in readable_spec)
 
-#     # embed parameters
-    
+   # set all parameters to zero
+   ps2.hidden1.W .= 0.0
 
-# end
+   # _map[spect] = index in readable_spec2
+   _map  = _invmap(readable_spec2)
+
+   # embed
+   for (idx, t) in enumerate(readable_spec)
+      ps2.hidden1.W[_map[t], :] = ps.hidden1.W[idx, :]
+   end
+
+   return ps2
+end
 
 # function VMC_multilevel_1d(opt_vmc::VMC, sam::MHSampler, ham::SumH, wf_list, ps_list, st_list, spec_list, spec1p_list, totaldegs, νs, verbose = true, accMCMC = [10, [0.45, 0.55]], sd_admissible = x -> true)
 
