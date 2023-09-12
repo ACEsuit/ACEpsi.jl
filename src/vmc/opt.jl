83b47f682670fccd07c3b9d172fda055a9718ee8
DexuanZhou <hbnis@icloud.com>
2023-09-12 14:25:57 +0800

    fix sr

--- src/vmc/opt.jl
+++ src/vmc/opt.jl
@@ -2,6 +2,17 @@ module vmc
 
 abstract type opt end
 
+abstract type sr_type end
+
+struct QGT <: sr_type
+end
+
+struct QGTJacobian <: sr_type
+end
+struct QGTOnTheFly <: sr_type
+end
+
+
 include("Eloc.jl")
 include("gradient.jl")
 include("metropolis.jl")
