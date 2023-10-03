92ade125acee1fc6bfcef224deefd7af3acd563d
cheukhinhojerry <cheukhinhojerry@gmail.com>
2023-10-03 14:19:48 -0700

    remove odd code

--- src/vmc/optimisers/adamw.jl
+++ src/vmc/optimisers/adamw.jl
@@ -8,18 +8,8 @@ end
 
 adamW() = adamW((9f-1, 9.99f-1), 0.0, eps())
 
-function Optimization(type::adamW, wf, ps, st, sam::MHSampler, ham::SumH, α)
-    λ₀, σ, E, x0, acc = Eloc_Exp_TV_clip(wf, ps, st, sam, ham)
-    g = grad(wf, x0, ps, st, E)
-    st_opt = Optimisers.setup(Optimisers.AdamW(α, type.β, type.γ, type.ϵ), ps)
-    st_opt, ps = Optimisers.update(st_opt, ps, g)
-    res = norm(destructure(g)[1])
-    return ps, acc, λ₀, res, σ, x0
-end
-
-
-function Optimization_parallel(type::adamW, wf, ps, st, sam::MHSampler, ham::SumH, α)
-    λ₀, σ, E, x0, acc = ACEpsi.vmc.Eloc_Exp_TV_clip_parallel(wf, ps, st, sam, ham)
+function Optimization(type::adamW, wf, ps, st, sam::MHSampler, ham::SumH, α; batch_size = 1)
+    λ₀, σ, E, x0, acc = Eloc_Exp_TV_clip(wf, ps, st, sam, ham; batch_size = batch_size)
     g = grad(wf, x0, ps, st, E)
     st_opt = Optimisers.setup(Optimisers.AdamW(α, type.β, type.γ, type.ϵ), ps)
     st_opt, ps = Optimisers.update(st_opt, ps, g)
